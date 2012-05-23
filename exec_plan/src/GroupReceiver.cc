bd52c2cddef49eb891bffef4c972c2bc091a930c
Marek Rogala <marrogala@gmail.com>
2012-05-23 15:52:10 +0200

    prace nad sender receive

---
+++ exec_plan/src/GroupReceiver.cc
@@ -0,0 +1,22 @@
+#include <cassert>
+
+#include "GroupReceiver.h"
+
+namespace Engine {
+
+GroupReceiver::GroupReceiver(NodeEnvironmentInterface *node_env, OperationTree::GroupByOperation &node) : 
+	node_env_(node_env), node_(node) {
+
+	}
+
+std::vector<void*> GroupReceiver::pull(int &rows) {
+
+
+
+}
+
+std::vector<OperationTree::ScanOperation_Type> GroupReceiver::init() {
+	assert(false);
+}
+
+}
---
+++ exec_plan/src/GroupSender.cc
@@ -0,0 +1,21 @@
+#include <cassert>
+
+#include "GroupSender.h"
+
+namespace Engine {
+
+GroupSender::GroupSender(NodeEnvironmentInterface *node_env, Operation *source, const OperationTree::GroupByOperation &node) : node_env_(node_env), 
+	source_(source), node_(node) {
+	}
+
+std::vector<void*> GroupSender::pull(int &rows) {
+
+	// this is dummy return value
+	return std::vector<void*>();
+}
+
+std::vector<OperationTree::ScanOperation_Type> GroupSender::init() {
+	assert(false);
+}
+
+}
