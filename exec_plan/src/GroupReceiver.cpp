c3197bc1ab55b35f149165727e023cfe83ab2a6d
Robert Rosołek <rr277585@students.mimuw.edu.pl>
2012-05-23 15:13:38 +0200

    group receiver group sender

--- exec_plan/src/GroupReceiver.cpp
+++ exec_plan/src/GroupReceiver.cpp
@@ -1,5 +1,11 @@
 #include "GroupReceiver.h"
 
+
+GroupReceiver(NodeEnvironmentInterface *node_env, OperationTree::GroupByOperation &node) : 
+	node_env_(node_env), node_(node) {
+
+	}
+
 std::vector<void*> GroupReceiver::pull(int &rows) {
 
 
--- exec_plan/src/GroupSender.cpp
+++ exec_plan/src/GroupSender.cpp
@@ -1,9 +1,14 @@
 #include "GroupSender.h"
 
+
+GroupSender(NodeEnvironmentInterface *node_env, Operation *source, const OperationTree::GroupByOperation &node) : node_env_(node_env), 
+	source_(source), node_(node) {
+	
+
+
+	}
+
 std::vector<void*> GroupSender::pull(int &rows) {
-
-
-
 
 	// this is dummy return value
 	return std::vector<void*>();
