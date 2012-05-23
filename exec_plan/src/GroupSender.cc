09f99b64dfcafd315a42c58660bbb15a396be296
Robert Rosolek <rr277585@students.mimuw.edu.pl>
2012-05-23 16:07:55 +0200

    dummy

--- exec_plan/src/GroupSender.cc
+++ exec_plan/src/GroupSender.cc
@@ -4,17 +4,31 @@
 
 namespace Engine {
 
-GroupSender::GroupSender(NodeEnvironmentInterface *node_env, Operation *source, const OperationTree::GroupByOperation &node) : node_env_(node_env), 
-	source_(source), node_(node) {
+GroupSender::GroupSender(NodeEnvironmentInterface *node_env, Operation *source, const OperationTree::GroupByOperation &node) : 
+	node_env_(node_env), source_(source), node_(node) {
+
 	}
 
+int32* countHashes(vector<void*> &data, int rows) {
+	
+}
+
 std::vector<void*> GroupSender::pull(int &rows) {
+
+	int rows = 0;
+	vector<void*> data;
+
+	//data = source_->pull(rows);
+	while (rows > 0) {
+		
+	}
 
 	// this is dummy return value
 	return std::vector<void*>();
 }
 
 std::vector<OperationTree::ScanOperation_Type> GroupSender::init() {
+
 	assert(false);
 }
 
