8fc0dbca5e2451ba887ca879b723ceffe9a157ae
Marek Rogala <marrogala@gmail.com>
2012-06-08 11:14:51 +0200

    poprawka layers.h ---> node 0 zarezerwowany na sink

--- exec_plan/src/layers.h
+++ exec_plan/src/layers.h
@@ -3,8 +3,9 @@
 
 #include "node_environment/node_environment.h"
 
-/* nodes 0..floor(n/2)-1   --> Layer number 0
-   nodes floor(n/2)..n-1   --> Layer number 1
+/* node 0                    --> gathers results at the end
+   nodes 1..floor(n/2)       --> Layer number 0
+   nodes floor(n/2)+1..n-1   --> Layer number 1
    
    Initially Layer 0 are receivers, Layer 1 senders
    */
@@ -15,9 +16,11 @@ int inline CountNodesInLayer(NodeEnvironmentInterface* nei, int layerNum){
   if(layerNum == 0){
     return nodes/2;
   } else {
-    return nodes - nodes/2;
+    return nodes - nodes/2 - 1;
   }
 }
+
+int inline WhoGathers(){ return 0; }
 
 int inline GetMyLayer(NodeEnvironmentInterface* nei){
   return nei->my_node_number() >= CountNodesInLayer(nei, 0);
@@ -33,6 +36,7 @@ int inline CountNodesInOtherLayer(NodeEnvironmentInterface* nei){
 
 void inline SendPacket(NodeEnvironmentInterface* nei, uint32 target_node,
               const char* data, int  data_len){
+  target_node++;
   if(GetMyLayer(nei) == 0) target_node += CountNodesInLayer(0);
   nei->SendPacket(target_node, data, data_len);
 }
